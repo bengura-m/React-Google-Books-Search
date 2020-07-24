ddc306e00011bf82c0d647fb37ffdb4983983b52
Mitchelle Bengura <bengura.mitchell@gmail.com>
2020-07-24 23:13:53 +0800

    added build folder

---
+++ client/build/precache-manifest.b5900f343e8742c17fd27c3b1309b0e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6102f4ef1da3a5b6ce96",
+    "url": "/static/css/main.0138b773.chunk.css"
+  },
+  {
+    "revision": "6102f4ef1da3a5b6ce96",
+    "url": "/static/js/main.907ddda8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d51d57ab41415776cc1",
+    "url": "/static/js/2.9bb83eaa.chunk.js"
+  },
+  {
+    "revision": "bd47d03a4dfb4fccc52a108bb6a8b545",
+    "url": "/index.html"
+  }
+];
